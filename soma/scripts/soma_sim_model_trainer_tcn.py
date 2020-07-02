fea69f037d00aa7fe6305ec02995f857943933bc
Michael Burke <michaelburke@ieee.org>
2020-07-02 10:14:28 +0100

    Added seed scripts for soma training

--- soma/scripts/soma_sim_model_trainer_tcn.py
+++ soma/scripts/soma_sim_model_trainer_tcn.py
@@ -1,52 +1,33 @@
+#!/usr/bin/python
 
-# coding: utf-8
-
-# In[1]:
-
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("seed", type=int, help="Set seed number here")
+args = parser.parse_args()
 
 import numpy as np
+np.random.seed(args.seed)
 from matplotlib import pyplot as plt
 from IPython import display
 import torch
 import torch.nn as nn
+torch.manual_seed(args.seed)
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from scipy.optimize import linear_sum_assignment
 
-np.random.seed(0)
-torch.manual_seed(0)
 from torch.nn.utils import weight_norm
 device = torch.device('cuda:0')
 
-
-# In[2]:
-
-
 key = {'Orange':0,'Green':1,'Black':2,'Purple':3,'White':4,'LBlue':5,'Blue':6}
 
-
-# In[3]:
-
-
 extraction_orders = np.genfromtxt('./extraction_order.txt',delimiter=',',dtype=str)
-
-
-# In[4]:
-
 
 images = np.load('cube_ims.npy')
 print (images.shape)
 
-
-# In[5]:
-
-
 actions = np.vectorize(key.get)(extraction_orders)
 print(actions.shape)
-
-
-# In[6]:
-
 
 K = 7
 a_one_hot = np.zeros((actions.shape[0],K,K))
@@ -54,10 +35,6 @@ for i,a in enumerate(actions):
     oh = np.zeros((K,K))
     oh[np.arange(a.shape[0]),a] = 1
     a_one_hot[i,:,:] = oh
-
-
-# In[7]:
-
 
 class Sampler(Dataset):
     
@@ -80,23 +57,12 @@ class Sampler(Dataset):
         return im.to(device), actions.to(device), torch.eye(self.K).to(device),self.indices[index].to(device)
 
 
-# In[8]:
-
-
 dataset = Sampler(np.swapaxes(np.stack(images),2,4),actions,7)
 
 train_dataset,test_dataset = torch.utils.data.random_split(dataset, [180,60])
 
-
-# In[9]:
-
-
 batch_size = 32
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-
-# In[10]:
-
 
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):
@@ -186,7 +152,6 @@ class TCNNet(nn.Module):
         
         self.tcn = TemporalConvNet(K,[K]*K)
         
-        # Sinkhorn params
         self.latent_dim = latent_dim
         self.K = K
         
@@ -212,19 +177,11 @@ class TCNNet(nn.Module):
 
         return self.criterion(seq_logits.view(-1,self.K),seq.view(-1)), seq_logits
 
-
-# In[11]:
-
-
 bc = TCNNet(latent_dim=1024, image_channels=12, K=7)
 bc.to(device)
 optimizer = torch.optim.Adam(bc.parameters(), lr=1e-4)
 n_epochs = 5000
 losses = []
-
-
-# In[12]:
-
 
 plt.figure(figsize=(15,7))
 
@@ -242,48 +199,10 @@ for j in range(n_epochs):
         optimizer.zero_grad()
         
     losses.append(np.mean(batch_losses))
-    
-    plt.clf()
-    plt.cla()
-    plt.subplot(1,3,1)
-    plt.plot(losses,alpha=0.5)
-    plt.title('Recon loss')
-    
-    plt.subplot(3,3,2)
-    plt.imshow(np.argmax(seq_pred[0,:].cpu().detach().numpy(),-1).reshape(1,-1))
-    plt.title('Re-shuffled seq')
-    
-    plt.subplot(3,3,5)
-    plt.imshow(np.argmax(seq_order[0,:].cpu().detach().numpy(),-1).reshape(1,-1))
-    plt.title('Ordered seq')
-    
-    plt.subplot(3,3,8)
-    plt.imshow(seq[0,:].cpu().detach().numpy().reshape(1,-1))
-    plt.title('Original seq')
-    
-    plt.subplot(2,3,3)
-    plt.imshow(np.swapaxes(im[0,0:3,:,:].cpu().detach().numpy(),0,2))
-    
-        
-    display.clear_output(wait=True)
-    display.display(plt.gcf())
-    
-
-
-# In[13]:
-
 
 bc.eval()
 
-
-# In[14]:
-
-
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
-
-# In[15]:
-
 
 prec_list = []
 actions_pred = []
@@ -297,36 +216,11 @@ for im,seq,seq_ordered,indices in test_loader:
     prec = np.sum(obj_ids==seq.cpu().numpy())/7
     prec_list.append(prec)
 
-plt.figure(figsize=(15,5))
-plt.plot(prec_list)
-plt.ylabel('Precision')
-plt.xlabel('Demonstration')
-plt.grid()
-plt.show()
-
-
-# In[16]:
-
-
-np.mean(prec_list)
-
-
-# In[17]:
-
-
 parts = [k[0] for k in key.items()]
-
-
-# In[18]:
-
 
 pred_extractions = np.array(parts)[np.array(actions_pred).astype(int)]
 indices = np.array(index_list).astype(int)
 
-
-# In[19]:
-
-
-np.save('pred_order_tcn.npy',pred_extractions)
-np.save('test_indices_tcn.npy',indices)
+np.save('pred_order_tcn_%02d.npy'%args.seed,pred_extractions)
+np.save('test_indices_tcn_%02d.npy'%args.seed,indices)
 
